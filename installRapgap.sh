dda2b187079437e0ee62cae14716e251a24834dc
Aude Glaenzer <56628371+aglaenze@users.noreply.github.com>
2021-03-08 15:40:49 +0100

    Update installRapgap.sh

--- installRapgap.sh
+++ installRapgap.sh
@@ -2,15 +2,18 @@
 
 export WORKDIR=$(pwd)
 
-wget --no-verbose --no-check-certificate http://www.hepforge.org/archive/rapgap/rapgap-3.301.tar.gz
+#version=3.301
+version=3.303
+#wget --no-verbose --no-check-certificate http://www.hepforge.org/archive/rapgap/rapgap-3.301.tar.gz
+wget --no-verbose --no-check-certificate https://rapgap.hepforge.org/downloads/?f=rapgap-3.303.tar.gz
 mkdir rapgap-install
 cp steer_test rapgap-install/
-tar xvfz rapgap-3.301.tar.gz
-rm rapgap-3.301.tar.gz
+tar xvfz rapgap-$version.tar.gz
+rm rapgap-$version.tar.gz
 
-cp rapgap-lib-alexander/* $WORKDIR/rapgap-3.301/src/rapgap/
+cp rapgap-lib-alexander/* $WORKDIR/rapgap-$version/src/rapgap/
 
-cd $WORKDIR/rapgap-3.301
+cd $WORKDIR/rapgap-$version
 
 #./configure --disable-shared --prefix=$WORKDIR/rapgap-install --with-pythia6=$WORKDIR/pythia6 --with-lhapdf="/afs/cern.ch/work/a/abylinki/public/lhapdf_build" --with-hepmc=$WORKDIR/HepMC/install CC=gcc-7 CXX=g++-7
 
